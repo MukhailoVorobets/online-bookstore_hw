c2b2526416fa12570ba3680ba5e0e36022797b83
Mikhailo <88093373+MukhailoVorobets@users.noreply.github.com>
2025-01-15 19:33:08 +0200

    Added DTOs for books, MapStruct, service layer, and controller.

--- src/main/java/com/example/model/Book.java
+++ src/main/java/com/example/model/Book.java
@@ -7,14 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import java.util.Objects;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
 @Table(name = "books")
-@Getter
-@Setter
+@Data
 public class Book {
 
     @Id
@@ -35,38 +32,4 @@ public class Book {
 
     private String description;
     private String coverImage;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Book book = (Book) o;
-        return Objects.equals(id, book.id) && Objects.equals(title, book.title)
-                && Objects.equals(author, book.author) && Objects.equals(isbn, book.isbn)
-                && Objects.equals(price, book.price)
-                && Objects.equals(description, book.description)
-                && Objects.equals(coverImage, book.coverImage);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, author, isbn, price, description, coverImage);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{"
-                + "id=" + id
-                + ", title='" + title + '\''
-                + ", author='" + author + '\''
-                + ", isbn='" + isbn + '\''
-                + ", price=" + price
-                + ", description='" + description + '\''
-                + ", coverImage='" + coverImage + '\''
-                + '}';
-    }
 }
