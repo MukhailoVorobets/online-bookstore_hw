f62f995873c1910e0408eb2d616bea298fae6fd4
Mikhailo <88093373+MukhailoVorobets@users.noreply.github.com>
2025-02-17 23:59:34 +0200

    Add book globalExceptionHandler and Data Validation

--- src/main/java/com/example/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/dto/CreateBookRequestDto.java
@@ -1,14 +1,24 @@
 package com.example.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
+    @Size(max = 1000)
     private String description;
+    @NotNull
     private String coverImage;
 }
