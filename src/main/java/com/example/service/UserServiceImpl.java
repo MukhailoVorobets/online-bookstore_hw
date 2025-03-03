47ce3f7b828eef1d8eb2501fabb0c77e7d787dd1
Mikhailo <88093373+MukhailoVorobets@users.noreply.github.com>
2025-03-03 23:17:09 +0200

    Spring Boot Security: added User entity

---
+++ src/main/java/com/example/service/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.service;
+
+import com.example.dto.UserRegistrationRequestDto;
+import com.example.dto.UserResponseDto;
+import com.example.exception.RegistrationException;
+import com.example.mapper.UserMapper;
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository repository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponseDto register(UserRegistrationRequestDto request)
+            throws RegistrationException {
+        if (repository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RegistrationException("Can't register User");
+        }
+
+        User user = userMapper.toModel(request);
+        return userMapper.toDto(repository.save(user));
+    }
+}
