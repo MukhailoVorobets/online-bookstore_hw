99e7f215b31796e58cdfef1c12541ec6cdf4b80e
Mikhailo <88093373+MukhailoVorobets@users.noreply.github.com>
2025-03-24 22:20:37 +0200

    Spring Boot Security: third homework has been added JWT.

--- src/main/java/com/example/controller/AuthenticationController.java
+++ src/main/java/com/example/controller/AuthenticationController.java
@@ -47,7 +47,7 @@ public class AuthenticationController {
             }
     )
     @PostMapping("/login")
-    public UserLoginResponseDto login(@RequestBody UserLoginRequestDto requestDto) {
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
         return authenticationService.authenticate(requestDto);
     }
 
